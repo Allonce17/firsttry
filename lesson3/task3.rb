4791a390a35df38c525b8f970e7c37a6be4c74cf
Allonce17 <120310033+Allonce17@users.noreply.github.com>
2023-01-10 17:02:27 +0300

    Update task3.rb

--- lesson3/task3.rb
+++ lesson3/task3.rb
@@ -1,17 +1,17 @@
 # Заполнить массив числами фибоначчи до 100
 first = 0
 second = 1
-next_tern = 0
+next_step = 0
 
 fib_amount = 11
 
 fibonacci = []
 
 fib_amount.times do
-    next_tern = first + second
+    next_step = first + second
     first = second
-    second = next_tern
-    fibonacci << next_tern
+    second = next_step
+    fibonacci << next_step
 
 end
 
