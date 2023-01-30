642ff0953c381b43a5a411f3cc9624aed5f3ed71
Allonce17 <120310033+Allonce17@users.noreply.github.com>
2023-01-30 15:29:13 +0300

    fix indents

--- lesson6/task2.rb
+++ lesson6/task2.rb
@@ -7,23 +7,23 @@
 # -Жаль, но из этого треугольник не сделать
 
 class TriangleChecker
-	attr_reader :side_a, :side_b, :side_c
-	def initialize(side_a, side_b, side_c)
-		@side_a = side_a
-		@side_b = side_b
-		@side_c = side_c
-	end	
-	def is_triangle
-        if side_a.to_f != side_a  || side_b.to_f != side_b || side_c.to_f != side_c
-        	puts 'Нужно вводить только числа!'
-        elsif side_a.negative? || side_b.negative? || side_c.negative?
-        	puts 'С отрицательными числами ничего не выйдет!'
-        elsif side_a > side_b + side_c || side_b > side_a + side_c || side_c > side_b + side_a
-        	puts 'Жаль, но из этого треугольник не сделать'   
-        else
-        	puts 'Ура, можно построить треугольник!'
-        end    
-	end	
+    attr_reader :side_a, :side_b, :side_c
+    def initialize(side_a, side_b, side_c)
+      @side_a = side_a
+      @side_b = side_b
+      @side_c = side_c
+    end	
+    def is_triangle
+      if side_a.to_f != side_a  || side_b.to_f != side_b || side_c.to_f != side_c
+        puts 'Нужно вводить только числа!'
+      elsif side_a.negative? || side_b.negative? || side_c.negative?
+        puts 'С отрицательными числами ничего не выйдет!'
+      elsif side_a > side_b + side_c || side_b > side_a + side_c || side_c > side_b + side_a
+        puts 'Жаль, но из этого треугольник не сделать'   
+      else
+        puts 'Ура, можно построить треугольник!'
+      end    
+    end	
 end
 tre = TriangleChecker.new(3, 5, 4)
 tre.is_triangle 	
